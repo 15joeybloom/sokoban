cc0ff8d9606461867120b03b0d3c9e13bb8c7f92
Joey Bloom <15joeybloom@gmail.com>
2017-06-13 05:27:09 -0700

    Work on undo. No tests, TDD fail.

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,7 +13,7 @@ module Lib
   ) where
 
 import Data.HashMap.Strict as H
-import Data.List
+import Data.List (findIndex)
 
 data Contents
   = Player
@@ -32,35 +32,6 @@ data Square
   | Wall
   deriving (Show, Eq)
 
-data Warehouse = WH
-  { map :: H.HashMap (Int, Int) Square
-  , width :: Int
-  , height :: Int
-  , player_row :: Int
-  , player_column :: Int
-  } deriving (Show, Eq)
-
-is_player (Space Player _) = True
-is_player _ = False
-
-make_warehouse :: [[Square]] -> Maybe Warehouse
-make_warehouse grid = do
-  pr <- findIndex (any is_player) grid
-  pc <- findIndex is_player $ grid !! pr
-  return $ WH map w h pr pc
-  where
-    map = foldl aux H.empty $ zip [0 ..] grid
-    aux h (r, xs) = foldl aux2 h $ zip [0 ..] xs
-      where
-        aux2 h (c, x) = H.insert (r, c) x h
-    w = foldl (flip $ max . length) 0 grid
-    h = length grid
-
-warehouse_dimensions wh = (width wh, height wh)
-
-get :: Warehouse -> Int -> Int -> Square
-get (WH map _ _ _ _) r c = H.lookupDefault Wall (r, c) map
-
 data Direction
   = North
   | South
@@ -74,27 +45,59 @@ data Move
   | NoMove
   deriving (Show, Eq)
 
+data Warehouse = WH
+  { whmap :: H.HashMap (Int, Int) Square
+  , width :: Int
+  , height :: Int
+  , player_row :: Int
+  , player_column :: Int
+  , move_stack :: [Move]
+  } deriving (Show, Eq)
+
+is_player (Space Player _) = True
+is_player _ = False
+
+make_warehouse :: [[Square]] -> Maybe Warehouse
+make_warehouse grid = do
+  pr <- findIndex (any is_player) grid
+  pc <- findIndex is_player $ grid !! pr
+  return $ WH newmap w h pr pc []
+  where
+    newmap = foldl aux H.empty $ zip [0 ..] grid
+    aux h (r, xs) = foldl aux2 h $ zip [0 ..] xs
+      where
+        aux2 h (c, x) = H.insert (r, c) x h
+    w = foldl (flip $ max . length) 0 grid
+    h = length grid
+
+warehouse_dimensions wh = (width wh, height wh)
+
+get :: Warehouse -> Int -> Int -> Square
+get warehouse r c = H.lookupDefault Wall (r, c) $ whmap warehouse
+
 step North r c = (r - 1, c)
 step South r c = (r + 1, c)
 step West r c = (r, c - 1)
 step East r c = (r, c + 1)
 
 move :: Direction -> Warehouse -> (Warehouse, Move)
-move dir warehouse@(WH map w h r c) =
+move dir warehouse@(WH oldmap w h r c stk) =
   case get warehouse r' c' of
     Space Empty d' ->
       let newmap =
             (H.insert (r, c) (Space Empty d) $
-             H.insert (r', c') (Space Player d') map)
-       in (WH newmap w h r' c', Walk dir)
+             H.insert (r', c') (Space Player d') oldmap)
+          move = Walk dir
+       in (WH newmap w h r' c' (move : stk), move)
     Space Box d' ->
       case get warehouse r'' c'' of
         Space Empty d'' ->
           let newmap =
-                (H.insert (r, c) (Space Empty d) $
-                 H.insert (r', c') (Space Player d') $
-                 H.insert (r'', c'') (Space Box d'') map)
-           in (WH newmap w h r' c', Push dir)
+                H.insert (r, c) (Space Empty d) $
+                H.insert (r', c') (Space Player d') $
+                H.insert (r'', c'') (Space Box d'') oldmap
+              move = Push dir
+           in (WH newmap w h r' c' (move : stk), move)
         _ -> (warehouse, NoMove)
     _ -> (warehouse, NoMove)
   where
@@ -102,8 +105,32 @@ move dir warehouse@(WH map w h r c) =
     (r', c') = step dir r c
     (r'', c'') = step dir r' c'
 
+one_eighty North = South
+one_eighty South = North
+one_eighty West = East
+one_eighty East = West
+
+undo :: Warehouse -> Warehouse
+undo warehouse@(WH {move_stack = (NoMove:moves)}) =
+  warehouse {move_stack = moves}
+undo warehouse@(WH {move_stack = (Walk dir:moves)}) =
+  moved_back {move_stack = moves}
+  where
+    moved_back = fst $ move (one_eighty dir) warehouse
+undo warehouse@(WH newmap w h r' c' (Push dir:moves)) = WH oldmap w h r c moves
+  where
+    oldmap =
+      H.insert (r, c) (Space Player d) $
+      H.insert (r', c') (Space Box d') $
+      H.insert (r'', c'') (Space Empty d'') newmap
+    Space Empty d = get warehouse r c
+    Space Player d' = get warehouse r' c'
+    Space Box d'' = get warehouse r'' c''
+    (r, c) = step (one_eighty dir) r' c'
+    (r'', c'') = step dir r' c'
+
 warehouse_solved :: Warehouse -> Bool
-warehouse_solved (WH map _ _ _ _) = all aux map
+warehouse_solved = all aux . whmap
   where
     aux (Space Box Dot) = True
     aux (Space _ Dot) = False
